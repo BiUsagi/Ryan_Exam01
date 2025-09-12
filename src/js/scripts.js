a2eec655d4d617f0e16af30ffe3c55e6995c6ab3
BiUsagi <tubanovel@gmail.com>
2025-09-12 19:39:13 +0700

    finish pc and reponsive sp

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,57 @@
-// Web Font Loader - Prevent flashing when loading web-fonts
-$(window).on("load", function () {
-  window.WebFontConfig = {
-    custom: {
-      families: ["Inter:n3,n4,n5,n6,n7,n8"],
-      urls: ["/css/top.css"],
+// // Web Font Loader - Prevent flashing when loading web-fonts
+// $(window).on("load", function () {
+//   window.WebFontConfig = {
+//     custom: {
+//       families: ["Inter:n3,n4,n5,n6,n7,n8"],
+//       urls: ["/css/top.css"],
+//     },
+//   };
+//   (function () {
+//     var wf = document.createElement("script");
+//     wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+//     wf.type = "text/javascript";
+//     wf.async = "true";
+//     var s = document.getElementsByTagName("script")[0];
+//     s.parentNode.insertBefore(wf, s);
+//   })();
+// });
+
+// Slider Case Study
+const swiper = new Swiper(".casestudy-content", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".slide-button-next",
+    prevEl: ".slide-button-prev",
+  },
+
+  pagination: {
+    el: ".casestudy-slide-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-  };
-  (function () {
-    var wf = document.createElement("script");
-    wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
-    wf.type = "text/javascript";
-    wf.async = "true";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// Slider Testimonial
+const swiperTestimonial = new Swiper(".testimonial-slider", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: ".testimnial-button-next",
+    prevEl: ".testimnial-button-prev",
+  },
 });
